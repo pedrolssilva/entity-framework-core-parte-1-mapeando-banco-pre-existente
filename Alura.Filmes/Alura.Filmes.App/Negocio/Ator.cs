75e3b85f010c7b250224b9223b33bb3261907fdb
pedrolssilva <pedrolssilva@hotmail.com>
2022-10-03 20:22:59 -0300

    Adjusted Ator entity to be possible replicate sql script based on EF Core

--- Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
+++ Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,20 @@ namespace Alura.Filmes.App.Negocio
     [Table("actor")]
     public class Ator
     {
+        [Column("actor_id")]
         public int Id { get; set; }
+
+        [Required]
+        [Column("first_name", TypeName = "varchar(45)")]
         public string PrimeiroNome { get; set; }
+
+        [Required]
+        [Column("last_name", TypeName = "varchar(45)")]
         public string UltimoNome { get; set; }
+
+        public override string ToString()
+        {
+            return $"Ator ({Id}): {PrimeiroNome} {UltimoNome}";
+        }
     }
 }
