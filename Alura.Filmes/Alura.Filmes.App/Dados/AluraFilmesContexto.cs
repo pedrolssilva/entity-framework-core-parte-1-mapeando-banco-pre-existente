192fba73e12bc2f40507a06d2f0f4e79988f38de
pedrolssilva <pedrolssilva@hotmail.com>
2022-10-03 18:58:13 -0300

    Created project commit

---
+++ Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,19 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=AluraFilmes;User Id=sa;Password=MSSQL_SA_123;");
+        }
+    }
+}
---
+++ Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Negocio
+{
+    [Table("actor")]
+    public class Ator
+    {
+        public int Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+    }
+}
