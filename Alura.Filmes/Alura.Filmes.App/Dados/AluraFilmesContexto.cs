1d66af5b19f2b10dccf58ab3538fe77b2a141a0d
pedrolssilva <pedrolssilva@hotmail.com>
2022-10-05 19:33:34 -0300

    Created Filme entity

--- Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContexto.cs
+++ Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -11,6 +11,8 @@ namespace Alura.Filmes.App.Dados
     public class AluraFilmesContexto : DbContext
     {
         public DbSet<Ator> Atores { get; set; }
+        public DbSet<Filme> Filmes{ get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost;Database=AluraFilmes;User Id=sa;Password=MSSQL_SA_123;");
@@ -25,13 +27,11 @@ namespace Alura.Filmes.App.Dados
                 .Property(a => a.Id)
                 .HasColumnName("actor_id");
 
-
             modelBuilder.Entity<Ator>()
                 .Property(a => a.PrimeiroNome)
                 .HasColumnName("first_name")
                 .HasColumnType("varchar(45)")
                 .IsRequired();
-
 
             modelBuilder.Entity<Ator>()
                 .Property(a => a.UltimoNome)
@@ -45,6 +45,37 @@ namespace Alura.Filmes.App.Dados
                 .HasDefaultValueSql("getdate()")
                 .IsRequired();
 
+            modelBuilder.Entity<Filme>()
+                .ToTable("film");
+
+            modelBuilder.Entity<Filme>()
+                .Property(f => f.Id)
+                .HasColumnName("film_id");
+
+            modelBuilder.Entity<Filme>()
+                .Property(f => f.Titulo)
+                .HasColumnName("title")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            modelBuilder.Entity<Filme>()
+                .Property(f => f.Descricao)
+                .HasColumnName("description")
+                .HasColumnType("text");
+
+            modelBuilder.Entity<Filme>()
+                .Property(f => f.AnoLancamento)
+                .HasColumnName("release_year")
+                .HasColumnType("varchar(4)");
+
+            modelBuilder.Entity<Filme>()
+                .Property(f => f.Duracao)
+                .HasColumnName("length");
+
+            modelBuilder.Entity<Filme>()
+                .Property<DateTime>("last_update")
+                .HasColumnType("datetime")
+                .IsRequired();
         }
     }
 }
